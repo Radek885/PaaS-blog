388e7e71bcd840ba1ea556472e926513741aac93
Radek885 <radekmaciuszonek@wp.pl>
2025-06-03 11:31:07 +0200

    no message

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 const express = require("express");
-const cors = require("cors");
 const { Pool } = require("pg");
 
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.use(cors());
 app.use(express.json());
 
 const pool = new Pool({
